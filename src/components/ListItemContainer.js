55f921dddd5da7e372f4bb1d53b995c3f44dcda9
youjae <youjae0803@gmail.com>
2023-08-27 15:40:45 +0900

    chore: update mainPage UI

--- src/components/ListItemContainer.js
+++ src/components/ListItemContainer.js
@@ -1,7 +1,8 @@
-import { ListItem, ListItemText } from "@mui/material";
+import { ListItem, ListItemText, Typography } from "@mui/material";
 import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import { IconButton } from "@mui/material";
 import { Delete } from "@mui/icons-material";
+import { hover } from "@testing-library/user-event/dist/hover";
 
 function ListItmeContainer(props) {
   const handleClick = (index) => {
@@ -21,11 +22,33 @@ function ListItmeContainer(props) {
         border: "1px solid black",
       }}
     >
-      <ListItemText
-        primary={props.index + 1 + ". " + props.item.content}
-      ></ListItemText>
-      <IconButton onClick={() => handleClick(props.item.habitId)} size="small">
-        <Delete sx={{ fontSize: 20 }} />
+      <ListItemText sx={{ color: "white", fontWeight: 400 }}>
+        <Typography fontSize={14} fontWeight={"bold"}>
+          {props.index + 1 + ". " + props.item.content}
+        </Typography>
+      </ListItemText>
+      <IconButton
+        onClick={() => handleClick(props.item.habitId)}
+        size="small"
+        sx={{
+          transition: "background-color 0.3s", // 변화를 부드럽게 만들기 위한 트랜지션 설정
+          "&:hover": {
+            backgroundColor: "#dbe5ebe5", // hover 시 배경색 변경
+            opacity: 0.8,
+            color: "black",
+          },
+        }}
+      >
+        <Delete
+          sx={{
+            fontSize: 20,
+            color: "white",
+            transition: "background-color 0.3s", // 변화를 부드럽게 만들기 위한 트랜지션 설정
+            "&:hover": {
+              color: "black", // hover 시 배경색 변경
+            },
+          }}
+        />
       </IconButton>
     </ListItem>
   );

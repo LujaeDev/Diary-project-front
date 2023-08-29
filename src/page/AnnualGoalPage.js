f79a873f19ac5fd22362cf2b7e7ffbe77e2161e5
youjae <youjae0803@gmail.com>
2023-08-29 18:16:54 +0900

    feat: make logic to save anuual goal

--- src/page/AnnualGoalPage.js
+++ src/page/AnnualGoalPage.js
@@ -5,7 +5,9 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function AnnualGoalPage() {
   const BoxRowStyle = styled("div")({
@@ -14,10 +16,86 @@ function AnnualGoalPage() {
     alignItems: "flex-start",
   });
 
+  const navigate = useNavigate();
   const [selectedDate, setSelectedDate] = useState(null);
+  const [careerList, setCarrerList] = useState([]);
+  const [healthList, setHealthList] = useState([]);
+  const [familyList, setFamilyList] = useState([]);
+  const [financeList, setFinanceList] = useState([]);
+  const categoryList = [
+    { category: "career", list: careerList, setList: setCarrerList },
+    { category: "health", list: healthList, setList: setHealthList },
+    { category: "family", list: familyList, setList: setFamilyList },
+    { category: "finance", list: financeList, setList: setFinanceList },
+  ];
+
+  useEffect(() => {
+    axios.defaults.headers.common["Authorization"] =
+      localStorage.getItem("token");
+    // Axios GET 요청 보내기
+
+    console.log(selectedDate);
+    categoryList.map((item) => {
+      const category = item.category;
+      const setList = item.setList;
+
+      const year = selectedDate === null ? 2023 : selectedDate.$y;
+      let uri = "/api/annualGoals?year=" + year + "&category=" + category;
+      const encodedURI = encodeURI(uri);
+      axios
+        .get(encodedURI, { headers: { Accept: "application/json" } })
+        .then((response) => {
+          // 성공적인 응답 처리
+          console.log("Response:", response.data);
+          setList(response.data.annualGoalResponseList);
+        })
+        .catch((error) => {
+          // 에러 핸들링 및 로그인 페이지로 리디렉션
+          console.error("Error: ", error);
+
+          if (error.response && error.response.status === 401) {
+            // 만약 에러 상태 코드가 401(Unauthorized)이면 로그인 페이지로 리디렉션
+            navigate("/"); // '/login'은 로그인 페이지의 경로로 수정해야 합니다.
+          }
+        });
+    });
+  }, [selectedDate]);
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
+  };
+
+  const addGoalHandler = (goalCategory, goalContent) => {
+    const year = selectedDate === null ? 2023 : selectedDate.$y;
+
+    var categoryObj = categoryList.filter((item) => {
+      return item.category === goalCategory;
+    })[0];
+
+    const formData = {
+      content: goalContent,
+      category: goalCategory,
+      year: year,
+    };
+
+    console.log(formData);
+    let uri = "/api/annualGoals";
+    axios
+      .post(uri, formData)
+      .then((response) => {
+        // 성공적인 응답 처리
+        categoryObj.setList([...categoryObj.list, response.data]);
+        console.log("Response:", response.data);
+      })
+      .catch((error) => {
+        // 에러 핸들링 및 로그인 페이지로 리디렉션
+        console.error("Error: ", error);
+
+        if (error.response && error.response.status === 401) {
+          // 만약 에러 상태 코드가 401(Unauthorized)이면 로그인 페이지로 리디렉션
+          navigate("/"); // '/login'은 로그인 페이지의 경로로 수정해야 합니다.
+        }
+      });
   };
 
   const content = (
@@ -35,13 +113,29 @@ function AnnualGoalPage() {
       </LocalizationProvider>
 
       <BoxRowStyle sx={{ marginTop: 4 }}>
-        <GoalCard title="Career" />
-        <GoalCard title="Health" />
+        <GoalCard
+          listGoals={careerList}
+          title="Career"
+          addHandler={addGoalHandler}
+        />
+        <GoalCard
+          listGoals={healthList}
+          title="Health"
+          addHandler={addGoalHandler}
+        />
       </BoxRowStyle>
 
       <BoxRowStyle sx={{ marginTop: 8 }}>
-        <GoalCard title="Family" />
-        <GoalCard title="Finance" />
+        <GoalCard
+          listGoals={familyList}
+          title="Family"
+          addHandler={addGoalHandler}
+        />
+        <GoalCard
+          listGoals={financeList}
+          title="Finance"
+          addHandler={addGoalHandler}
+        />
       </BoxRowStyle>
     </div>
   );
